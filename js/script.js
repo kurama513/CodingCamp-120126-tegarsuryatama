659e5c4b24b4e4065239c9d060006049ef45497e
Tegar Suryatama <tegarsuryatama53@gmail.com>
2026-01-14 19:19:25 +0700

    project 1

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 // Welcome message
-const namePrompt = prompt("Masukkan nama Anda:");
-if (namePrompt) {
+const userName = prompt("Please enter your name:");
+if (userName) {
   document.getElementById("welcomeText").innerText =
-    "Hi " + namePrompt + ", Welcome To Website";
+    `Hi ${userName}, Welcome To Website`;
 }
 
-// Form submit
+// Form handling
 document.getElementById("messageForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
   const name = document.getElementById("name").value;
-  const birthdate = document.getElementById("birthdate").value;
+  const birth = document.getElementById("birthdate").value;
   const message = document.getElementById("messageText").value;
   const gender = document.querySelector('input[name="gender"]:checked').value;
 
-  const now = new Date().toString();
-
-  document.getElementById("currentTime").innerText = now;
+  document.getElementById("currentTime").innerText = new Date().toString();
   document.getElementById("resultName").innerText = name;
-  document.getElementById("resultBirth").innerText = birthdate;
+  document.getElementById("resultBirth").innerText = birth;
   document.getElementById("resultGender").innerText = gender;
   document.getElementById("resultMessage").innerText = message;
 });
